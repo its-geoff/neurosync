12251c4ce5d6f12897de8966d5fd6095d089944d
its-geoff <geoffrey.agustin04@gmail.com>
2025-11-11 22:22:38 -0800

    converted data to pandas dataframe, added tabular form of data output

--- software/src/data_processing.py
+++ software/src/data_processing.py
@@ -1,6 +1,19 @@
-import csv
-import matplotlib
 import os
+import pandas as pd
+from tabulate import tabulate
+from scipy.fft import fft
+import matplotlib
+
+"""
+[x] extract data from csv
+[x] print data from four channels and combine data
+[ ] convert from mV to Hz
+[ ] split data based on wave frequency
+[ ] put each frequency into separate data structure
+[ ] run preliminary tests on each frequency, checking for min, max, and variance
+[ ] create alternative output format that is human readable (ex: table)
+[ ] output data structures in desired output format
+"""
 
 def get_data(file_name):
     """
@@ -15,7 +28,31 @@ def get_data(file_name):
     folder_name = os.path.abspath("..\data")
     return os.path.join(folder_name, file_name)
 
+def transform_to_hz(data):
+    """
+    Converts EEG input in mV to frequency in Hz.
+
+    Gamma - >32 Hz (Not needed for emotions; filter out)
+    ----------------
+    Beta - 13-32 Hz
+    Alpha - 8-13 Hz
+    Theta - 4-8 Hz
+    Delta - 0.5-4 Hz
+
+    Notes:
+        - keep rows intact to show emotional state at a point in time
+        - channels do matter; frontal channels more important in emotional detection
+        - don't remove or combine channels arbitrarily
+
+    Arguments:
+        data (pandas DataFrame): The CSV file data in mV to be converted to Hz.
+    
+    Returns:
+
+    """
+
 def main():
+    # change to get_data(file) later with file being an arg in main
     file_path = get_data("muse2_eeg_data.csv");
 
     # check file existence
@@ -23,11 +60,12 @@ def main():
         print(f"file does not exist at: {file_path}")
         return
 
-    with open(file_path, newline='') as csvfile:
-        line = csv.reader(csvfile, delimiter=' ')
+    # prints first five rows of readings, split by channel
+    df = pd.read_csv(file_path)
+    df_channels = df[["ch1", "ch2", "ch3", "ch4"]]
+    print(tabulate(df_channels.head(), headers='keys', tablefmt='grid', showindex=False))
 
-        for row in line:
-            print(', '.join(row))
+
 
 if __name__ == '__main__':
     main()
