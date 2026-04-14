6853325d0d4e826b0d8d30cfea9dc08fa2af3b10
its-geoff <109061107+its-geoff@users.noreply.github.com>
2026-04-14 00:25:24 -0700

    created baseline graph and delta updates correctly

--- software/src/graphing.py
+++ software/src/graphing.py
@@ -5,7 +5,9 @@ Graphs brainwave band data dynamically.
 
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 from matplotlib.lines import Line2D
+from matplotlib.axes import Axes
 
 
 def create_figure():
@@ -17,17 +19,18 @@ def create_figure():
     Returns:
         tuple [Line2D, ...]: Line objects that represent a brainwave frequency.
     """
+    plt.ion()   # turn on interactive mode
     fig, ax = plt.subplots(4, 1)
-    line_delta = ax[0].plot([], [])
-    line_theta = ax[1].plot([], [])
-    line_alpha = ax[2].plot([], [])
-    line_beta = ax[3].plot([], [])
+    line_delta, = ax[0].plot([], [])
+    line_theta, = ax[1].plot([], [])
+    line_alpha, = ax[2].plot([], [])
+    line_beta, = ax[3].plot([], [])
     
     plt.show()
-    return line_delta, line_theta, line_alpha, line_beta
+    return fig, ax, line_delta, line_theta, line_alpha, line_beta
 
 
-def update_delta(line: Line2D, fft_df: pd.DataFrame):
+def update_delta(line: Line2D, ax: Axes, fft_df: pd.DataFrame):
     """Updates the current line on the graph.
 
     Arguments:
@@ -39,15 +42,33 @@ def update_delta(line: Line2D, fft_df: pd.DataFrame):
     """
     line.set_xdata(fft_df["timestamp"].values)
     line.set_ydata(fft_df["delta"].values)
-    plt.draw()
+    ax.relim()
+    ax.autoscale_view()
+    line.figure.canvas.draw()
+    line.figure.canvas.flush_events()
 
 
 def run(fft_df: pd.DataFrame):
     """Creates a graph and constantly updates the graph when new data is
     added."""
-    line_delta, line_theta, line_alpha, line_beta = create_figure()
-    update_delta(line_delta, fft_df)
+    fig, ax, line_delta, line_theta, line_alpha, line_beta = create_figure()
+    update_delta(line_delta, ax[0], fft_df)
+    
+    for i in range(1, len(fft_df) + 1):
+        update_delta(line_delta, ax[0], fft_df.iloc[:i])
+        plt.pause(0.1)
+        
+    plt.ioff()  # turn off interactive mode
+    plt.show()  # blocking commands until window closed
 
 
 if __name__ == "__main__":
-    run()
+    fft_df = pd.DataFrame({
+        "timestamp": np.arange(100),
+        "delta": np.random.uniform(1, 100, 100),
+        "theta": np.random.uniform(1, 100, 100),
+        "alpha": np.random.uniform(1, 100, 100),
+        "beta":  np.random.uniform(1, 100, 100),
+    })
+    
+    run(fft_df)
